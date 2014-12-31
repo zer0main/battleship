ad818b91c1805d1ef788c80d7199d168512d3253
coder <pdolgov99@gmail.com>
2014-12-31 18:45:28 +0300

    New structure Points
    Points contains cell coordinates;
    add header Points.hpp

---
+++ src/Points.hpp
@@ -0,0 +1,19 @@
+/*
+ * Battleship, classic battleship game
+ * Copyright (C) 2014 Pavel Dolgov
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef POINTS_HPP_
+#define POINTS_HPP_
+
+/** Structure contains cell coordinates.
+*/
+struct Point {
+    /** Vertical coordinate */
+    int col;
+    /** Horizontal coordinate */
+    int row;
+};
+
+#endif
