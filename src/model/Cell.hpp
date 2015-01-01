100e82eb70c4c7c7c98a51302b1eeb75b46e8a66
coder <pdolgov99@gmail.com>
2015-01-01 19:36:35 +0300

    New structure Cell

---
+++ src/model/Cell.hpp
@@ -0,0 +1,24 @@
+/*
+ * Battleship, classic battleship game
+ * Copyright (C) 2014 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef CELL_HPP_
+#define CELL_HPP_
+
+/** Structure Cell contains all information about cell.
+*/
+struct Cell {
+    /** is_ship is true if ship/part of ship situated in
+    this cell
+    */
+    bool is_ship;
+    /** is_visible is true if enemy can see what is
+    situated in this cell
+    */
+    bool is_visible;
+};
+
+#endif
