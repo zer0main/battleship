dfe141aaf8602432f8c47d80e1310795a2d8afa7
Pavel Dolgov <pdolgov99@gmail.com>
2015-01-06 13:07:14 +0300

    Add static function getIndex
    Get global coordinate from vertical and horizontal
    coordinates. Use getIndex in this methods of GameDesk:
    getVisibility, setVisibility, getCellState,
    setCellState.

--- src/model/GameDesk.cpp
+++ src/model/GameDesk.cpp
@@ -7,49 +7,51 @@
 
 #include "GameDesk.hpp"
 
+/* get global coordinate from horizontal and
+   vertical coordinates
+*/
+static int getIndex(const Point& point, int width) {
+    int index = point.col * width + point.row;
+    return index;
+}
+
 void GameDesk::setCellState(const Point& point,
         bool state, bool player) {
-    /* get global coordinate from horizontal and
-       vertical coordinates
-    */
-    int global_coordinate = point.col * width_ + point.row;
+    int index = getIndex(point, width_);
     if (player) {
-        players_desk_[global_coordinate].is_ship = state;
+        players_desk_[index].is_ship = state;
     } else {
-        enemys_desk_[global_coordinate].is_ship = state;
+        enemys_desk_[index].is_ship = state;
     }
 }
 
 bool GameDesk::getCellState(const Point& point,
         bool player) const {
+    int index = getIndex(point, width_);
     if (player) {
-        return players_desk_[point.col * width_
-            + point.row].is_ship;
+        return players_desk_[index].is_ship;
     } else {
-        return enemys_desk_[point.col * width_
-            + point.row].is_ship;
+        return enemys_desk_[index].is_ship;
     }
 }
 
 void GameDesk::setVisibility(const Point& point, bool
         state, bool player) {
-    int global_coordinate = point.col * width_ + point.row;
+    int index = getIndex(point, width_);
     if (player) {
-        players_desk_[global_coordinate].is_visible =
-            state;
+        players_desk_[index].is_visible = state;
     } else {
-        enemys_desk_[global_coordinate].is_visible = state;
+        enemys_desk_[index].is_visible = state;
     }
 }
 
 bool GameDesk::getVisibility(const Point& point,
         bool player) const {
+    int index = getIndex(point, width_);
     if (player) {
-        return players_desk_[point.col * width_
-            + point.row].is_visible;
+        return players_desk_[index].is_visible;
     } else {
-        return enemys_desk_[point.col * width_
-            + point.row].is_visible;
+        return enemys_desk_[index].is_visible;
     }
 }
 
