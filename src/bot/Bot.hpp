a1b2c27664eb5b45a84712a9b49eb350a6347fc4
Pavel Dolgov <pdolgov99@gmail.com>
2015-01-16 23:18:31 +0300

    New class Bot
    Bot class contains all bot logic.

---
+++ src/bot/Bot.hpp
@@ -0,0 +1,30 @@
+/*
+ * Battleship, classic battleship game
+ * Copyright (C) 2014-2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef GAME_DESK_HPP_
+#define GAME_DESK_HPP_
+
+#include "GameDesk.hpp"
+
+/** Bot class contains all bot logic.
+*/
+class Bot {
+public:
+    /** Set pointer to model class GameDesk
+    */
+    void setDesk(const GameDesk* desk);
+
+    /** This method returns Point structure which contains
+    coordinates of cell that bot wants to shoot
+    */
+    Point getIndex() const;
+
+private:
+    const GameDesk* desk_;
+};
+
+#endif
