5eb85fa4b488c6b67620784bef7360fc9bc2fb39
Pavel Dolgov <pdolgov99@gmail.com>
2015-02-07 20:58:02 +0300

    Add all methods of GameDeskProxy to class header

--- src/model/GameDeskProxy.hpp
+++ src/model/GameDeskProxy.hpp
@@ -21,6 +21,35 @@ public:
     */
     GameDeskProxy(const GameDesk* desk, int player_number);
 
+    /** Get state of cell specified in Point structure.
+    \param player_number Defines player(1 or 2) on
+    whose territory current cell is situated
+    */
+    bool getCellState(const Point& point,
+                      int player_number) const;
+
+    /** Returns true if sunken ship (or part of sunken
+    ship) situated in cell specified in Point structure.
+    \param player_number Defines player(1 or 2) on
+    whose territory current cell is situated
+    */
+    bool getFlooding(const Point& point,
+                     int player_number) const;
+
+    /** Get visibility of cell specified in Point
+    structure.
+    \param player_number Defines player(1 or 2) on
+    whose territory current cell is situated
+    */
+    bool getVisibility(const Point& point,
+                       int player_number) const;
+
+    /** Get width of game board */
+    int getWidth() const;
+
+    /** Get length of game board */
+    int getLength() const;
+
 private:
     const GameDesk* desk_;
 
