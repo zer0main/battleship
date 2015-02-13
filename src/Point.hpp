6f99826f3754a5ff6c62757093b02dea1c56de53
Pavel Dolgov <pdolgov99@gmail.com>
2015-02-13 22:59:21 +0300

    New structure Points
    Use to specify coordinates of end and beginning of ship.

--- src/Point.hpp
+++ src/Point.hpp
@@ -18,4 +18,16 @@ struct Point {
     int row;
 };
 
+/** Structure Points contains 2 Point structuries.
+Use to specify coordinate of the beginning of ship (p1) and
+coordinate of the end of ship (p2).
+*/
+struct Points {
+    /** Beginning of ship */
+    Point p1;
+
+    /** End of ship */
+    Point p2;
+};
+
 #endif
