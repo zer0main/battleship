027a9b9aef7322742b2c3867e8c3a0a2ab1c3f43
Pavel Dolgov <pdolgov99@gmail.com>
2015-03-06 14:00:55 +0300

    Copyright line

--- src/view/main.cpp
+++ src/view/main.cpp
@@ -1,3 +1,10 @@
+/*
+ * Battleship, classic battleship game
+ * Copyright (C) 2014-2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
 #include <QtGui/QApplication>
 #include "mainwindow.h"
 
