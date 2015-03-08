a259987b5534bf691f86950f3031f7b046c7c100
Pavel Dolgov <pdolgov99@gmail.com>
2015-03-08 19:40:57 +0300

    Use cmake instead of qmake

---
+++ src/view/TableModel.hpp
@@ -0,0 +1,38 @@
+/*
+ * Battleship, classic battleship game
+ * Copyright (C) 2014-2015 Pavel Dolgov
+ *
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef TABLE_MODEL_HPP_
+#define TABLE_MODEL_HPP_
+
+#include <QtGui>
+
+#include "GameDeskProxy.hpp"
+
+/** Model for QTableView */
+class TableModel : public QAbstractTableModel {
+    Q_OBJECT
+public:
+    TableModel(QObject* parent,
+               const GameDeskProxy* desk,
+               bool hostile);
+
+    int rowCount(const QModelIndex& parent =
+                 QModelIndex()) const;
+
+    int columnCount(const QModelIndex& parent =
+                    QModelIndex()) const;
+
+    QVariant data(const QModelIndex& index, int role =
+                  Qt:DisplayRole) const;
+
+private:
+    const GameDeskProxy* desk_;
+
+    bool hostile_;
+};
+
+#endif
