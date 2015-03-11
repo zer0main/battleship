9e9059403b8322823ca320bf56f0b6882c261627
Pavel Dolgov <pdolgov99@gmail.com>
2015-03-12 01:36:12 +0300

    srand in main()

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,16 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <cstdlib>
+#include <ctime>
+
 #include <QtGui/QApplication>
+
 #include "MainWindow.hpp"
 
 int main(int argc, char* argv[])
 {
+    srand(time(NULL));
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
