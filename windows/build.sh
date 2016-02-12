8895ffe88f0b3a30c80ba4c2fc47d39ddb244e51
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-12 19:03:19 +0300

    Add build.sh for windows

---
+++ windows/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xue
+
+MXE_DIR=/usr/lib/mxe
+
+if [ "$FILENAME_SUFFIX" = "windows32" ]; then
+    MXE_TARGET=i686-w64-mingw32.static
+fi
+
+if [ "$FILENAME_SUFFIX" = "windows64" ]; then
+    MXE_TARGET=x86-64-w64-mingw32.static
+fi
+
+$MXE_DIR/usr/bin/MXE_TARGET-cmake . -Bbuild-dir
+cmake --build build-dir --config Release
+build-dir/rename_exe.sh
+
+set +xue
