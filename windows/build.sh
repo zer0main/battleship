88b12df6069a5ab61c76ee368b63826c9ced32db
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-12 22:49:15 +0300

    MXE_TARGET: x86_64 --> x86-64

--- windows/build.sh
+++ windows/build.sh
@@ -12,7 +12,10 @@ if [ "$FILENAME_SUFFIX" = "windows64" ]; then
     MXE_TARGET=x86-64-w64-mingw32.static
 fi
 
-$MXE_DIR/usr/bin/$MXE_TARGET-cmake . -Bbuild-dir
+# x86_64 -> x86-64
+MXE2_TARGET=$(echo "$MXE_TARGET" | sed 's/_/-/g')
+
+$MXE_DIR/usr/bin/$MXE2_TARGET-cmake . -Bbuild-dir
 cmake --build build-dir --config Release
 source build-dir/rename_exe.sh
 
