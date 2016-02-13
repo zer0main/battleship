c98e9f74f1521cc56272bea4f3cf11dd71ed1521
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-13 16:53:23 +0300

    windows/build.sh: correct MXE_TARGET for win64

--- windows/build.sh
+++ windows/build.sh
@@ -9,11 +9,8 @@ if [ "$FILENAME_SUFFIX" = "windows32" ]; then
 fi
 
 if [ "$FILENAME_SUFFIX" = "windows64" ]; then
-    MXE_TARGET=x86-64-w64-mingw32.static
+    MXE_TARGET=x86_64-w64-mingw32.static
 fi
-
-# x86_64 -> x86-64
-MXE2_TARGET=$(echo "$MXE_TARGET" | sed 's/_/-/g')
 
 $MXE_DIR/usr/bin/$MXE2_TARGET-cmake . -Bbuild-dir
 cmake --build build-dir --config Release
