417a2460d95f51c8d4ceae4cba2c792e3f0bb509
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-13 22:36:33 +0300

    Use @ONLY; use @ with PROJECT_SOURCE_DIR

--- build/windows/package.sh
+++ build/windows/package.sh
@@ -5,14 +5,14 @@ set -xue
 strip $DEPLOY_FILE
 upx -9 $DEPLOY_FILE
 
-cp ${PROJECT_SOURCE_DIR}/src/view/images/battleship.ico build-dir
+cp @PROJECT_SOURCE_DIR@/src/view/images/battleship.ico build-dir
 
-awk 'sub("$", "\r")' ${PROJECT_SOURCE_DIR}/README.md > \
+awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/README.md > \
     build-dir/README.txt
-awk 'sub("$", "\r")' ${PROJECT_SOURCE_DIR}/LICENSE > \
+awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/LICENSE > \
     build-dir/LICENSE.txt
 
-awk 'sub("$", "\r")' ${PROJECT_SOURCE_DIR}/AUTHORS > \
+awk 'sub("$", "\r")' @PROJECT_SOURCE_DIR@/AUTHORS > \
     build-dir/AUTHORS.txt
 
 if [ -f "build-dir/wizard.nsi" ]; then
