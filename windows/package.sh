5a1a47c3661ccbd4e716bcdddaf3b143a7c6163f
Pavel Dolgov <pdolgov99@gmail.com>
2016-02-13 18:13:44 +0300

    Mv some commands from nsis_wizard.sh to package.sh

--- windows/package.sh
+++ windows/package.sh
@@ -4,6 +4,17 @@ set -xue
 
 strip $DEPLOY_FILE
 upx -9 $DEPLOY_FILE
+
+cp ${PROJECT_SOURCE_DIR}/src/view/images/battleship.ico build-dir
+
+awk 'sub("$", "\r")' ${PROJECT_SOURCE_DIR}/README.md > \
+    build-dir/README.txt
+awk 'sub("$", "\r")' ${PROJECT_SOURCE_DIR}/LICENSE > \
+    build-dir/LICENSE.txt
+
+awk 'sub("$", "\r")' ${PROJECT_SOURCE_DIR}/AUTHORS > \
+    build-dir/AUTHORS.txt
+
 if [ -f "build-dir/wizard.nsi" ]; then
     build-dir/nsis_wizard.sh
 fi
