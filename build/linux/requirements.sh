673d9e72af1b251c66c276aa7db2da007f5fff59
Mike Kinney <mike.kinney@gmail.com>
2016-04-19 00:45:41 -0700

    are you really, really sure you want to upgrade?

--- build/linux/requirements.sh
+++ build/linux/requirements.sh
@@ -6,7 +6,7 @@ sudo rm -rf /var/lib/apt/lists
 
 sudo apt-get update
 
-sudo apt-get upgrade -y
+sudo apt-get --force-yes upgrade -y
 
 sudo apt-get --yes install \
     g++ make cmake \
--- build/windows/requirements.sh
+++ build/windows/requirements.sh
@@ -11,7 +11,7 @@ sudo apt-key adv --keyserver x-hkp://keys.gnupg.net \
 
 sudo apt-get update
 
-sudo apt-get upgrade -y
+sudo apt-get --force-yes upgrade -y
 
 sudo apt-get --yes install upx-ucl
 
